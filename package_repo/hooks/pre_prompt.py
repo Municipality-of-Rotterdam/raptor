22c6a5cdd18da4d5456becb0bc4b95fef41a39be
Jasper Siebring <j.siebring@rotterdam.nl>
2026-03-02 16:15:39 +0000

    Refactored main.yml to use separate pipeline yaml for tagging functionality

--- package_repo/hooks/pre_prompt.py
+++ package_repo/hooks/pre_prompt.py
@@ -8,8 +8,8 @@ toml_path = raptor_root / "pyproject.toml"
 cookiecutter_config_path = repo_root / "cookiecutter.json"
 
 # reading raptor version
-raptor_config = tomllib.load(open(toml_path, 'rb'))
-raptor_version = raptor_config['project']['version']
+raptor_config = tomllib.load(open(toml_path, "rb"))
+raptor_version = raptor_config["project"]["version"]
 
 # setting template version and dumping it back to JSON
 cookiecutter_config = json.loads(cookiecutter_config_path.read_text())
